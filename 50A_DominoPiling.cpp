a495905621aff8adcd84588d7ea8030a8272b7d5
Subhey Sadi <srahman212074@bscse.uiu.ac.bd>
2022-08-18 14:11:18 +0600

    Updated

--- 50A_DominoPiling.cpp
+++ 50A_DominoPiling.cpp
@@ -4,9 +4,7 @@ using namespace std;
 
 int main(){
     int n, m;
-    cin >> n;
-    cin >> m;
-    int temp = n * m;
-    cout << ceil(temp / 2) << endl;
+    cin >> m >> n;
+    cout << (n * m) / 2 << endl;
     return 0;
 }
