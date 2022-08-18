70fa341e0b5ae699e280c750d55b5d59ace7881c
Subhey Sadi <srahman212074@bscse.uiu.ac.bd>
2022-08-18 14:03:43 +0600

    50A Domino Piling

---
+++ 50A_DominoPiling.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n;
+    cin >> m;
+    int temp = n * m;
+    cout << ceil(temp / 2) << endl;
+    return 0;
+}
